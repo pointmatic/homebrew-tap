07f48a89d07ef1d9da322fecba187f9297abcc14
Michael Smith <pointmatic@gmail.com>
2026-04-17 17:01:47 -0700

    formula for gitbetter v1.1.0

--- Formula/gitbetter.rb
+++ Formula/gitbetter.rb
@@ -6,35 +6,25 @@ class Gitbetter < Formula
   license "Apache-2.0"
 
   def install
-#    libexec.install "lib", "gitbetter.sh", "git-push.sh", "git-tag.sh"
-#    (bin/"gitbetter").write <<~SH
-#      #!/usr/bin/env bash
-#      exec "#{libexec}/gitbetter.sh" "$@"
-#    SH
-#    (bin/"git-push").write <<~SH
-#      #!/usr/bin/env bash
-#      exec "#{libexec}/git-push.sh" "$@"
-#    SH
-#    (bin/"git-tag").write <<~SH
-#      #!/usr/bin/env bash
-#      exec "#{libexec}/git-tag.sh" "$@"
-#    SH
-#    chmod 0555, bin/"gitbetter", bin/"git-push", bin/"git-tag"
-
-    # v1.0.0 only
-    bin.install "git-push.sh" => "git-push"
-    bin.install "git-tag.sh"  => "git-tag"
- 
+    libexec.install "lib", "gitbetter.sh", "git-push.sh", "git-tag.sh"
+    (bin/"gitbetter").write <<~SH
+      #!/usr/bin/env bash
+      exec "#{libexec}/gitbetter.sh" "$@"
+    SH
+    (bin/"git-push").write <<~SH
+      #!/usr/bin/env bash
+      exec "#{libexec}/git-push.sh" "$@"
+    SH
+    (bin/"git-tag").write <<~SH
+      #!/usr/bin/env bash
+      exec "#{libexec}/git-tag.sh" "$@"
+    SH
+    chmod 0555, bin/"gitbetter", bin/"git-push", bin/"git-tag"
   end
 
   test do
-#    assert_match "v#{version}", shell_output("#{bin}/gitbetter --version")
-#    assert_match "v#{version}", shell_output("#{bin}/git-push --version")
-#    assert_match "v#{version}", shell_output("#{bin}/git-tag --version")
-
-    # v1.0.0 only
-    assert_predicate bin/"git-push", :exist?
-    assert_predicate bin/"git-tag",  :exist?
-    
+    assert_match "v#{version}", shell_output("#{bin}/gitbetter --version")
+    assert_match "v#{version}", shell_output("#{bin}/git-push --version")
+    assert_match "v#{version}", shell_output("#{bin}/git-tag --version")
   end
 end
