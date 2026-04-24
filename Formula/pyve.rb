c1e9161778d4741e64dd71d31fde2876371af4eb
Michael Smith <pointmatic@users.noreply.github.com>
2026-04-23 19:28:41 -0700

    pyve 2.3.0 (#28)

--- Formula/pyve.rb
+++ Formula/pyve.rb
@@ -1,8 +1,8 @@
 class Pyve < Formula
   desc "A single, easy entry point for Python virtual environments"
   homepage "https://pointmatic.github.io/pyve"
-  url "https://github.com/pointmatic/pyve/archive/refs/tags/v2.2.1.tar.gz"
-  sha256 "91a4e811faa182cb65d015ddba338f71b537d3b0fd501e7e08f6d413b2400ca0"
+  url "https://github.com/pointmatic/pyve/archive/refs/tags/v2.3.0.tar.gz"
+  sha256 "4d0e0d5b903806e8a570a411cca5880f40d28a78e3809df5801f919c018f93b2"
   license "Apache-2.0"
 
   def install
