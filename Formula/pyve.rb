0c3a63347b83c37e4fbd028512f1a514a662fe2d
Michael Smith <pointmatic@users.noreply.github.com>
2026-06-15 09:11:25 -0700

    pyve 3.0.7 (#50)

--- Formula/pyve.rb
+++ Formula/pyve.rb
@@ -1,8 +1,8 @@
 class Pyve < Formula
   desc "Single, easy entry point for all your virtual environments"
   homepage "https://pointmatic.github.io/pyve"
-  url "https://github.com/pointmatic/pyve/archive/refs/tags/v3.0.6.tar.gz"
-  sha256 "775a890fb7be067c6e1280012fca84622a3a801dfcbbf94fb92ac66824a599c4"
+  url "https://github.com/pointmatic/pyve/archive/refs/tags/v3.0.7.tar.gz"
+  sha256 "020a083445876ade7c5c2af8a9a4293c9580fa30a148cb4bc59a67dba6f0c2c6"
   license "Apache-2.0"
 
   # tomllib (used by pyve) requires Python >= 3.11. This brew Python only
